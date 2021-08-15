3e979038074b824066b0cd592cdde6648d0c9bb0
jayashreepu <88489428+jayashreepu@users.noreply.github.com>
2021-08-15 22:26:02 +0530

    Created List Methods.py

---
+++ listmethods.py
@@ -0,0 +1,134 @@
+# fruits = ["apple", "banana", "cherry"]
+# fruits.append("watermelon")
+# print(fruits)
+
+# numbers = ["14", "01", "03"]
+# numbers.append("21")
+# print(numbers)
+
+# alphabets = ["a", "b", "c"]
+# alphabets.append("d")
+# print(alphabets)
+
+# fruits = ["apple", "banana", "cherry"]
+# fruits.clear()
+# print(fruits)
+
+# numbers = ["14", "01", "03"]
+# numbers.clear()
+# print(numbers)
+
+# alphabets = ["d", "e", "f"]
+# alphabets.clear()
+# print(alphabets)
+
+# fruits = ["apple", "banana", "cherry"]
+# x = fruits.copy()
+# print(x)
+
+# names = ["Vijayashree", "Teena", "Yashu"]
+# x = names.copy()
+# print(x)
+
+# numbers = ["14", "01", "03"]
+# x = numbers.copy()
+# print(x)
+
+# fruits = ["apple", "banana", "cherry"]
+# x = fruits.count("cherry")
+# print(x)
+
+# fruits = ["apple", "banana", "banana"]
+# x = fruits.count("banana")
+# print(x)
+
+# names = ["pinky", "pinky", "pinky"]
+# x = names.count(inky)
+# print(x)
+
+# fruits = ['appple', 'banana', 'cherry']
+# cars = ['Ford', 'BMW', 'Volvo']
+# fruits.extend(cars)
+# print(fruits)
+
+# numbers = ['1', '2', '3']
+# names = ['inky', 'pinky', 'ponky']
+# numbers.extend(names)
+# print(numbers)
+
+# cars = ['innova', 'scorpio', 'fortuner']
+# names = ['jay', 'vijay', 'ajay']
+# cars.extend(names)
+# print(cars)
+
+# fruits = ['apple', 'banana', 'cherry']
+# x = fruits.index("cherry")
+# print(x)
+
+# fruits = ["apple", "banana", "watermelon"]
+# x = fruits.index("watermelon")
+# print(x)
+
+# names = ["inky", "pinky", "ponky"]
+# x = names.index("inky")
+# print(x)
+
+# fruits = ["apple", "banana", "cherry"]
+# fruits.insert(1, "orange")
+# print(x)
+
+# names = ["inky", "ponky", "teena"]
+# names.insert(1, "pinky")
+# print(names)
+
+# numbers = ["1", "3", "4"]
+# numbers.insert(2, "2")
+# print(numbers)
+
+# fruits = ["apple", "banana", "cherry"]
+# fruits.pop(1)
+# print(fruits)
+
+# names = ["Swetha", "Geetha", "Latha"]
+# names.pop(2)
+# print(names)
+
+# alphabets = ["J", "K", "L"]
+# alphabets.pop(0)
+# print(alphabets)
+
+# fruits = ["apple", "banana", "cherry"]
+# fruits.remove("banana")
+# print(fruits)
+
+# names = ["inky", "donkey", "pinkey"]
+# names.remove("donkey")
+# print(names)
+
+# numbers = ["14", "15", "16"]
+# numbers.remove("15")
+# print(numbers)
+
+# fruits = ["apple", "banana", "cherry"]
+# fruits.reverse()
+# print(fruits)
+
+# names = ["Greeshma", "Teena", "Vijayashree"]
+# names.reverse()
+# print(names)
+
+# numbers = ["19", "18", "17"]
+# numbers.reverse()
+# print(numbers)
+
+# cars = ["Ford", "BMW", "Volvo"]
+# cars.sort()
+# print(cars)
+
+# names = ["Vijayashree", "Teena", "Greeshma"]
+# names.sort()
+# print(names)
+
+# numbers = ["66", "61", "63"]
+# numbers.sort()
+# print(numbers)
